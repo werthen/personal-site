fa7cc3d195a4559467370f29fd7b95cc32d79cad
lorin <werthenlorin@gmail.com>
2022-09-05 12:03:51 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {}}:
 let
   build-deps = import ./packages.nix;
-in pkgs.stdenv.mkDerivation rec {
-  nativeBuildInputs = build-deps;
+in pkgs.stdenv.mkDerivation {
+  buildInputs = build-deps;
   name = "werthen.com";
   src = ./.;
   buildPhase = ''
